3560ff88017a991d1411cf35512b430d5f2c136c
Denis Yurashku <dayfuaim@gmail.com>
2015-11-03 23:45:13 +0300

    Update DenisYurashkuFindIndex.pm
    Another way of finding the index... Hm-hm-hum... :)

--- DenisYurashkuFindIndex.pm
+++ DenisYurashkuFindIndex.pm
@@ -34,4 +34,18 @@ sub find {
 	}
 }
 
+sub find1 {
+	my $self = shift;
+	my $x = shift; # Number to find index for
+	my $arr = shift; # Ref to array of numbers where to find the index
+	my $max = $#$arr;
+	my $i = 0;
+	$self->{STEPS} = 1;
+	return [0, $self->{STEPS}] if ($x<=$arr->[0]); # Before 0 (or 0) of ARRAY
+	$self->{STEPS}++;
+	return [$max, $self->{STEPS}] if ($x>=$arr->[$max]); # Farther than (or exactly at) LEN() of ARRAY
+	my @arr1 = sort { $a->[1] <=> $b->[1] || $a->[0] <=> $b->[0] } map { [$i++, abs($x-$_)] } @$arr;
+	return [$arr1[0]->[0],$self->{STEPS}]
+}
+
 1;
